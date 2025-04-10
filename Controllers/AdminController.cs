7e33a06b2c7d0378c9e81bb45abeaa808fd5a5f8
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-04-11 00:32:07 +0100

    added api endpoint to get all users

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,29 @@
-﻿namespace MarketplaceAPI.Controllers
+﻿using MarketplaceAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketplaceAPI.Controllers
 {
-    public class AdminController
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet("users")]
+        public IActionResult GetAllUsers()
+        {
+            var users = _userManager.Users;
+            return Ok(users);
+        }
     }
 }
