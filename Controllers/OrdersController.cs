fbc8344e9c1906652fc75d7715069e426c40f69b
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-04-11 00:24:31 +0100

    made get order function

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -1,18 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketplaceAPI.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MarketplaceAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class OrdersController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+
+        public OrdersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet]
-        public IActionResult GetOrders() => Ok("All orders");
+        public IActionResult GetOrders()
+        {
+            // admin can see all orders but user only it's own
+            if (User.IsInRole("Admin"))
+            {
+                var orders = _db.Orders.ToList();
+                return Ok(orders);
+            }
+            else
+            {
+                var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var orders = _db.Orders.Where(o => o.BuyerId == currentUserId).ToList();
+                return Ok(orders);
+            }
+        }
 
-        [HttpGet("{id}")]
-        public IActionResult GetOrder(int id) => Ok($"Order {id}");
-
-        [HttpPost]
-        public IActionResult CreateOrder() => Ok("Create order");
     }
 }
