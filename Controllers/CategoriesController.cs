1974d73edf48cb9374f8828f5dc659f70c32c365
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2025-04-17 10:13:12 +0100

    Create Ctegories Controller

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using MarketplaceAPI.Data;
+using MarketplaceAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace MarketplaceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var list = _db.Categories.ToList();
+            return Ok(list);
+        }
+    }
+}
