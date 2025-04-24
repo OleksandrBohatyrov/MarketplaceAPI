f70d7863b032bb12b557b996f9a50bc5550a4715
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-04-24 23:40:03 +0300

    dev

--- Program.cs
+++ Program.cs
@@ -119,7 +119,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    context.Database.Migrate();
+    //context.Database.Migrate();
 
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>(); 
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
