26b19c1857b6a3bdf7946b0012e7db04d4bb833e
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2025-04-25 01:53:12 +0300

    Create Modul for tags

---
+++ Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketplaceAPI.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        public ICollection<ProductTag> ProductTags { get; set; }
+            = new List<ProductTag>();
+    }
+}
