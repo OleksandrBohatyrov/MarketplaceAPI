a16de1862fed3cd1ea5fd859cfc6789b733c8879
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-04-25 21:40:32 +0300

    added migration

--- Program.cs
+++ Program.cs
@@ -115,6 +115,13 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+
+    context.Database.Migrate();
+
+
+
+
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
