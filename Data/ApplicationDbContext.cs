cba2c46f30211f1012d3ecc20a4d5abb5682e0aa
OleksandrBohatyrov <alexboggg361@gmail.com>
2025-05-05 09:19:41 +0300

    fixed comments

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,8 +12,8 @@ namespace MarketplaceAPI.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Tag> Tags { get; set; }                // <-- Новая таблица тегов
-        public DbSet<ProductTag> ProductTags { get; set; } // <-- Новая таблица связи «многие-ко-многим»
+        public DbSet<Tag> Tags { get; set; }                
+        public DbSet<ProductTag> ProductTags { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
@@ -22,7 +22,6 @@ namespace MarketplaceAPI.Data
         {
             base.OnModelCreating(builder);
 
-            // Конфигурация связи Product ↔ Tag через таблицу ProductTag
             builder.Entity<ProductTag>()
                 .HasKey(pt => new { pt.ProductId, pt.TagId });
 
